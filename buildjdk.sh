f4149f79046e867bbb5d65f17713a095662605a2
Tungstend <74163103+Tungstend@users.noreply.github.com>
2024-01-29 15:20:28 +0800

    Update buildjdk.sh

--- buildjdk.sh
+++ buildjdk.sh
@@ -31,7 +31,8 @@ if [ "$BUILD_IOS" != "1" ]; then
   platform_args="--with-toolchain-type=gcc \
     --with-freetype-include=$FREETYPE_DIR/include/freetype2 \
     --with-freetype-lib=$FREETYPE_DIR/lib \
-    --build=x86_64-unknown-linux-gnu \"
+    --build=x86_64-unknown-linux-gnu \
+    "
   AUTOCONF_x11arg="--x-includes=$ANDROID_INCLUDE/X11"
   AUTOCONF_EXTRA_ARGS+="OBJCOPY=$OBJCOPY \
     AR=$AR \
